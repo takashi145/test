3420d17ed1ff7f308173948fb6c63ebf411effd3
takashi145 <t.shuhei0203@gmail.com>
2023-09-05 09:43:40 +0900

    Create card component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,42 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import Card from './components/Card.vue';
+
+interface Item {
+  url: string
+  image: string
+  title: string
+}
+
+const items = ref<Item[]>([]);
+
+const fetchItems = async () => {
+  const response = await fetch('/src/data/items.json');
+  const data = await response.json();
+  return data;
+}
+
+onMounted(async () => {
+  items.value = await fetchItems();
+});
 </script>
 
 <template>
-  <div>
-    aaa
+  <div class="bg-white dark:bg-gray-800 min-h-screen">
+    <div class="py-12 px-8">
+      <div class="sm:columns-2 md:columns-3">
+        <div 
+          v-for="(item, index) in items" 
+          :key="index" 
+          class="mx-2 mb-8 max-w-lg"
+        >
+          <Card 
+            :url="item.url" 
+            :image="item.image"
+            :title="item.title"
+          />
+        </div>
+      </div>  
+    </div>
   </div>
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+  url: string
+  image: string
+  title: string
+}
+
+const props = defineProps<Props>();
+</script>
+<template>
+  <div class="max-w-sm mx-auto border border-gray-600 group/card transform transition-all duration-500 hover:scale-105 shadow-lg overflow-hidden hover:rounded-lg hover:ring-2">
+    <img :src="props.image" alt="" class="w-full">
+    <a :href="props.url" class="absolute block inset-0 group-hover/card:bg-black group-hover/card:bg-opacity-60 transition-all delay-150">
+      <h4 class="absolute bottom-0 p-3 font-semibold text-lg hidden group-hover/card:block text-white w-full">{{ props.title }}</h4>
+    </a>
+  </div>
+</template>
